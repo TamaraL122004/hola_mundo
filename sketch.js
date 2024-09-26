4397d9b1c18b8673b9dc6b6c2c13e982a55c0b33
TamaraL122004 <tamara.lamas@ucuenca.edu.ec>
2024-09-26 15:57:24 -0500

    Se ha creado un fondo y un cursor

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  frameRate(4);
+  background(0);
+  rectMode(CENTER);
+}
+
+function draw() {
+  background(random(150, 250), random(0, 100), 90, 50);
+
+  strokeWeight(9);
+  stroke(0);
+  fill(mouseY, 0, 150);
+  circle(mouseX + 70, mouseY, mouseX);
+
+  stroke(100, 100, 0);
+  fill(100, 0, 30);
+  rect(mouseX, mouseY, 110, 110);
+}
